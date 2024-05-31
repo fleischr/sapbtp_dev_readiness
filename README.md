efe0d360533196c44ba5851c7bf7c216018c4b3b
Ryan Fleischmann <ryfleisch@gmail.com>
2024-05-31 16:02:18 -0400

    Evaluates Phases I-V

--- README.md
+++ README.md
@@ -1,22 +1,70 @@
-# SAP BTP Development CLI and Automation Tools
+# The Batman Test for SAP BTP Development CLI, SDKs, and other Automation Tools
 
-Ordinary developers get their job done typing code and clicking buttons here and there.
+### How to use
+Open bash command line and run
 
-Extraordinary developers accelerate and automate
+```./batman```
 
-### Yeoman generator
+### Command line tools checked for (and why they're included!)
 
-### BTP CLI
+To acquire the Bat-signal, you will need to have these CLIs and other tools ready. 
 
-### Cloud Foundry CLI
+You do not have to know how they work! You just need to have them installed.
 
-### UI5 CLI
+I've included a brief summary and download/reference links as applicable
 
-### CAP CLI
+<b>Git</b>
+Use git to version control your apps!
 
-### abaplint CLI
+<b>Node.js</b>
 
-### MTA CLI
+<b>NPM</b>
 
+<b>Yeoman</b>
+Yeoman is hand for automated creation of Fiori and SAP BTP apps
+
+
+<b>UI5 CLI</b> 
+
+<b>MTA tools MBT </b>
+
+<b>abaplint</b>
+If you know you know!
+
+
+<b>CAP CDS</b>
+
+<b>Cloud Foundry Tools</b>
+
+<b>BTP CLI</b>
+
+<b>Python</b>
+Python is very commonly used for AI applications among a broder 
+
+<b>PIP</b>
+PIP is the package manager for Python. It can be easily missed during
+
+<b>Java</b>
+Java is 
+
+
+<b>OpenSSL</b>
+Every SAP BTP developer should have cryptography tools
+
+<b>Docker</b>
+
+<b>Kubernetes</b>
+
+### Easter eggs!
+
+You get the bat signal for installing CLIs and other tools generally considered essential for SAP BTP development and administration.
+
+But the fact is - it doesn't end there! There are many other really remarkable platforms you can bring alongside BTP. They're not really mandatory in any way - just compelling to know about
+
+ Explore additional CLI tools for cloud-native, AI, IoT, edge compute, and blockchain uses.
+
+### Contributing
+
+If there's command line tools you think I overlooked for SAP BTP developers - feel free to open an issue or pull request
 
 
--- batman.sh
+++ batman.sh
@@ -40,6 +40,9 @@ BG_WHITE="\e[47m"
 BOLD="\e[1m"
 UNDERLINE="\e[4m"
 
+echo -e "${BOLD} Phase I: Checking for Git, Node, and NPM ${RESET}"
+echo ""
+
 # Check if git is installed
 if command_exists git; then
     echo -e "${GREEN}Git is installed.${RESET}"
@@ -71,7 +74,7 @@ echo ""
 
 
 
-echo -e "${BOLD}Checking and possibly adding CLIs for BTP dev available via NPM...${RESET}"
+echo -e "${BOLD} Phase II: Checking and possibly adding CLIs for BTP dev available via NPM...${RESET}"
 echo ""
 
 if command_exists yo; then
@@ -118,6 +121,8 @@ echo -e "${BOLD}Checking status of other CLIs you should have for SAP BTP develo
 echo ""
 
 show_batman=0
+
+echo -e "${BOLD}Phase III: Checking for other CLIs and tools often needed for SAP BTP development"
 
 # Check if Cloud Foundry CLI is installed
 cf_installed=0
@@ -174,11 +179,13 @@ else
 fi
 echo ""
 
+echo -e "${BOLD} Phase IV : Checking for SAP BTP tools used for Kyma, DevSecOps, and IaaS ${RESET}"
+
 # Check if openssl is installed
 openssl_installed=0
 if command_exists openssl; then
     echo -e "${GREEN}openssl is installed.${RESET}"
-    java --version
+    openssl version
     openssl_installed=1
 else
     echo -e "${RED}Openssl is not installed.${RESET}"
@@ -194,11 +201,47 @@ if command_exists docker; then
 else
     echo -e "${RED}Docker is not installed.${RESET}"
 fi
+echo ""
 
-if ([ "$cf_installed" -eq 1 ] && [ "$btpcli_installed" -eq 1 ] && [ "$python_installed" -eq 1 ] && [ "$pip_installed" -eq 1 ] && [ "$java_installed" -eq 1 ] && [ "$openssl_installed" -eq 1 ] && [ "$docker_installed" -eq 1 ]); then
+kubernetes_installed=0
+if command_exists kubectl; then
+    echo -e "${GREEN}Kubernetes is installed.${RESET}"
+    kubectl version
+    kubernetes_installed=1
+else
+    echo -e "${RED}Kubernetes is not installed.${RESET}"
+fi
+echo ""
+
+golang_installed=0
+if command_exists go; then
+    echo -e "${GREEN}Golang is installed.${RESET}"
+    go version
+    golang_installed=1
+else
+    echo -e "${RED}Golang is not installed.${RESET}"
+fi
+echo ""
+
+terraform_installed=0
+if command_exists terraform; then
+    echo -e "${GREEN}Terraform is installed.${RESET}"
+    terraform version
+    terraform_installed=1
+else
+    echo -e "${RED}Terraform is not installed.${RESET}"
+fi
+echo ""
+
+if ([ "$cf_installed" -eq 1 ] && [ "$btpcli_installed" -eq 1 ] && [ "$python_installed" -eq 1 ] && [ "$pip_installed" -eq 1 ] && [ "$java_installed" -eq 1 ] ); then
     show_batman=1
 else
     show_batman=0
+fi
+
+show_muscle=0
+if ([ "$show_batman" -eq 1 ] && [ "$openssl_installed" -eq 1 ] && [ "$docker_installed" -eq 1 ] && [ "$kubernetes_installed" -eq 1 ] && [ "$golang_installed" -eq 1 ] && [ "$terraform_installed"  -eq 1 ] ); then
+    show_muscle=1
 fi
 
 if [ "$show_batman" -eq 1 ]; then
@@ -215,15 +258,8 @@ if [ "$show_batman" -eq 1 ]; then
     echo -e "${BOLD} Congratulations! You are now like Batman in SAP BTP development!${RESET}"
     echo ""
 
-    show_muscle=0
-    show_gigachad=0
-
-
-
-
-
     if [ "$show_muscle" -eq 1 ]; then
-    echo -e "${CYAN} And you installed these other CLIs for cloud native, edge compute, blockchain and AI development too? You really went the extra mile. I'm impressed!${RESET}"
+    echo -e "${CYAN} And you installed all the other Phase IV CLIs for Kubernetes, Kyma, DevSecOps, and IaaS? You really went the extra mile. I'm impressed!${RESET}"
     echo -e "\e[36m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⠿⡏⢿⡟⠿⠁⢸⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀
@@ -241,21 +277,119 @@ if [ "$show_batman" -eq 1 ]; then
     ⠀⠛⠛⠋⠁⠐⠛⠛⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\e[0m"
     echo ""
 
-        if [ "$show_gigachad" -eq 1 ]; then
-        echo "AND YOU GOT ##ALL## THE SECRET ONES TOO?!? You are a ..."
-        echo ""
-        echo "~ c e r t i f i e d ~"
-        echo "
-        
-    ██████╗ ██╗ ██████╗  █████╗  ██████╗██╗  ██╗ █████╗ ██████╗ 
-    ██╔════╝ ██║██╔════╝ ██╔══██╗██╔════╝██║  ██║██╔══██╗██╔══██╗
-    ██║  ███╗██║██║  ███╗███████║██║     ███████║███████║██║  ██║
-    ██║   ██║██║██║   ██║██╔══██║██║     ██╔══██║██╔══██║██║  ██║
-    ╚██████╔╝██║╚██████╔╝██║  ██║╚██████╗██║  ██║██║  ██║██████╔╝
-    ╚═════╝ ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
-        "
-        echo ""
-        fi
+    echo -e "${BOLD} Phase V: Bonus round! ${RESET}"
+
+    gcloud_exists=0
+    if command_exists gcloud; then
+        gcloud_exists=1
+        echo -e "You got Google Cloud CLI? Nice!"
+    else
+        echo -e "Try the Google Cloud CLI sometime :)"
+    fi
+    echo "~"
+
+    aws_exists=0
+    if command_exists aws; then
+        aws_exists=1
+        echo -e "You got AWS CLI? Nice!"
+    else
+        echo -e "Try the AWS CLI sometime :)"
+    fi
+    echo "~"
+
+    azure_exists=0
+    if command_exists az; then
+        azure_exists=1
+        echo -e "You got Azure CLI? Nice!"
+    else
+        echo -e "Try the Azure CLI sometime :)"
+    fi
+    echo "~"
+
+    ibmcloud_exists=0
+    if command_exists ibmcloud; then
+        ibmcloud_exists=1
+        echo -e "You got IBM Cloud CLI? Nice!"
+    else
+        echo -e "Try the IBM Cloud CLI sometime :)"
+    fi
+    echo "~"
+
+    openshift_exists=0
+    if command_exists oc; then
+        openshift_exists=1
+        echo -e "You got Openshift? You are true cloud pro!"
+    else
+        echo -e "Try the Red Hat Open Shift CLI sometime for cnative apps :)"
+    fi
+    echo "~"
+
+    nats_exists=0
+    if command_exists nats; then
+        nats_exists=1
+    else
+        echo -e "Try the NATs CLI sometime for edge compute :)"
+    fi
+    echo "~"
+
+    hardhat_exists=0
+    if command_exists hardhat; then
+        hardhat_exists=1
+        echo -e "Yes officer? It's this guy. He installed HARDHAT!"
+    else
+        echo -e "Try the Hardhat CLI sometime for smart contracts/blockchain :)"
+    fi
+    echo "~"
+
+    llama_exists=0
+    if command_exists ollama; then
+        llama_exists=1
+        echo -e "Mark would to thank you for trying Llama!"
+    else
+        echo -e "Try the Lllama CLI sometime for AI :)"
+    fi
+    echo "~"
+
+    rustup_exists=0
+    if command_exists rustup; then
+        rustup_exists=1
+        echo -e "aha. you like rust too?"
+    else
+        echo -e "Try building in Rust someday :)"
+    fi
+    echo "~"
+
+    cargo_exists=0
+    if command_exists cargo; then
+        cargo_exists=1
+        echo -e "Glad you got cargo too. Rust is good! I'd build SAP T/5 HANA with it - but thats just my opinion"
+    else
+        echo -e "Let me remind you again : Try building in Rust someday :)"
+    fi
+    echo "~"
+
+    show_gigachad=0
+    if ( [ "$gcloud_exists" -eq 1 ] && [ "$aws_exists" -eq 1 ] && [ "$azure_exists" -eq 1 ] && [ "$ibmcloud_exists" -eq 1 ] && [ "$openshift_exists" -eq 1 ] && [ "$nats_exists" -eq 1 ] && [ "$hardhat_exists"  -eq 1 ] && [ "$llama_exists" -eq 1 ] && [ "$rustup" -eq 1 ] && [ "$cargo_exists" -eq 1] ); then
+        show_gigachad=1
+    fi
+    echo ""
+
+    if [ "$show_gigachad" -eq 1 ]; then
+    echo "AND YOU GOT ##ALL## THE SECRET ONES TOO?!? You are a ..."
+    echo ""
+    echo "~ c e r t i f i e d ~"
+    echo "
+    
+██████╗ ██╗ ██████╗  █████╗  ██████╗██╗  ██╗ █████╗ ██████╗ 
+██╔════╝ ██║██╔════╝ ██╔══██╗██╔════╝██║  ██║██╔══██╗██╔══██╗
+██║  ███╗██║██║  ███╗███████║██║     ███████║███████║██║  ██║
+██║   ██║██║██║   ██║██╔══██║██║     ██╔══██║██╔══██║██║  ██║
+╚██████╔╝██║╚██████╔╝██║  ██║╚██████╗██║  ██║██║  ██║██████╔╝
+╚═════╝ ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
+    "
+    echo ""
+    fi
+
     fi
 
 else
@@ -296,6 +430,11 @@ else
         echo "Install docker" 
     fi
 
+    if [ "$kubernetes_installed" -eq 0 ]; then
+        echo "Install Kubernetes"
+    fi
 
-    echo -e "${MAGENTA} You should now have browser windows for how and where to install the extra tools you should have"
+    if [ "$golang_installed" -eq 0 ]; then
+        echo "Install Golang"
+    fi
 fi
